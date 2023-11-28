eb4e01830b49c7f517cb4dfc066f36ed19ce4393
Koulie123 <129706433+Koulie123@users.noreply.github.com>
2023-11-28 10:59:48 -0500

    Add project files.

---
+++ InterestCalculator/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+using InterestCalculator.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace InterestCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var viewModel = new CompoundInterestModelYear();
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Index(CompoundInterestModelYear viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                viewModel.SimplenterestCalculationTotal();
+            }
+            return View(viewModel);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ InterestCalculator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InterestCalculator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
