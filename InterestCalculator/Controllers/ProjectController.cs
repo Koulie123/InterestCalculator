9b2c71b3b692612cba872f235215821a986aa5b2
Koulie123 <129706433+Koulie123@users.noreply.github.com>
2023-12-01 09:46:27 -0500

    Started adding new pages to the project section and added controller for the section.

---
+++ InterestCalculator/Controllers/ProjectController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InterestCalculator.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Project1()
+        {
+            return View();
+        }
+    }
+}
